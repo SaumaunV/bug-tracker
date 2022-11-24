5b119cb41941bb8525875ce2e013d07da079b763
Saumaun <svahedipour1@gmail.com>
2022-11-23 21:15:53 -0600

    implemented auth0 on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
-import Head from 'next/head'
-import LoginForm from '../components/LoginForm'
+import { useUser } from "@auth0/nextjs-auth0";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Dashboard from "../components/Dashboard/Dashboard";
+import LoginForm from "../components/LoginForm";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  const router = useRouter();
+
+  if(user) router.push("/dashboard");
+  else if(isLoading) {}
+  else router.push("http://localhost:3000/api/auth/login");
+  
+  
   return (
     <div>
-      <LoginForm />
     </div>
-  )
+  );
 }
