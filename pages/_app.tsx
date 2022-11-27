c3a254f5733062fcd7c65872220a85f9b0ac68da
Saumaun <svahedipour1@gmail.com>
2022-11-26 18:24:58 -0600

    added auth0 provider to root file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Head>
-        <title>Bug Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider>
+        <Head>
+          <title>Bug Tracker</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserProvider>
   );
 }
