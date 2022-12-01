01998f460009b43d0ba8084fe801b7b404476321
Saumaun <svahedipour1@gmail.com>
2022-11-30 19:24:26 -0600

    added apollo client and provider to root component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,25 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http:localhost:4000/graphql',
+  cache: new InMemoryCache(),
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <ChakraProvider>
-        <Head>
-          <title>Bug Tracker</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </UserProvider>
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <ChakraProvider>
+          <Head>
+            <title>Bug Tracker</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </UserProvider>
+    </ApolloProvider>
   );
 }
