9312e52f98da3a5ac347e5bd0bb903860515e728
Saumaun <svahedipour1@gmail.com>
2022-12-06 22:26:57 -0600

    updated uri for apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { UserProvider } from "@auth0/nextjs-auth0";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http:localhost:4000/graphql',
+  uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 })
 
