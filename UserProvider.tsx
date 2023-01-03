b18903c6f80985634bae2b4a1c549a47a58738e3
Saumaun <svahedipour1@gmail.com>
2023-01-03 17:07:45 -0600

    UserProvider component
    Created global context state to store user information

---
+++ UserProvider.tsx
@@ -0,0 +1,55 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+type User = {
+    id: string;
+    username: string;
+    email: string;
+    role: string;
+} | null
+
+type ContextValue = {
+  user: User;
+  loading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
+};
+
+interface Props {
+    children?: React.ReactNode;
+}
+
+export const UserContext = createContext<ContextValue>({
+  user: null,
+  loading: false,
+  setUser: () => null,
+});
+
+
+export default function UserProvider({ children }: Props) {
+    const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState<User>(null);
+
+    useEffect(() => {
+        const getUser = async () =>{ 
+            setLoading(true);
+            try {
+                const resp = await fetch("http://localhost:4000/user", {
+                  credentials: "include"
+                });
+                const respData: User = await resp.json();
+                setUser(respData);
+            } catch (error) {
+                console.log(error);
+            }
+            setLoading(false);
+        }
+        getUser();
+
+    }, [])
+  return (
+    <UserContext.Provider value={{user, loading, setUser}}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export const useUser = () => useContext(UserContext);
