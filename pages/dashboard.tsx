afafc8b6125112afeedae26a9c37a4008b063419
Saumaun <svahedipour1@gmail.com>
2023-01-10 19:34:10 -0600

    Updated Dashboard page. Moved components to layout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,22 +1,15 @@
-import { useUser } from '@auth0/nextjs-auth0';
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
-import DashboardCharts from '../components/Dashboard/DashboardCharts';
-import DashboardInfo from '../components/Dashboard/DashboardInfo';
-import Header from '../components/Header';
-import NavMenu from '../components/NavMenu';
+import { Box} from "@chakra-ui/react";
+import React from "react";
+import DashboardCharts from "../components/Dashboard/DashboardCharts";
+import DashboardInfo from "../components/Dashboard/DashboardInfo";
 
 function Dashboard() {
   return (
-    <Flex>
-      <NavMenu />
-      <Flex direction="column" flex={1}>
-        <Header />
-        <DashboardInfo />
-        <DashboardCharts />
-      </Flex>
-    </Flex>
+    <Box>
+      <DashboardInfo />
+      <DashboardCharts />
+    </Box>
   );
 }
 
-export default Dashboard
+export default Dashboard;
