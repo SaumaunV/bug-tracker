43a83a61552b8c54afac10ab19d0853bfaf06e38
Saumaun <svahedipour1@gmail.com>
2023-01-15 23:00:50 -0600

    Added admin page to manage users.

---
+++ pages/admin.tsx
@@ -0,0 +1,40 @@
+import { useQuery } from '@apollo/client';
+import { Flex, Table, TableContainer, Tbody, Th, Thead, Tr, useColorMode } from '@chakra-ui/react';
+import React from 'react'
+import User from '../components/User';
+import { GET_USERS } from '../graphql/queries';
+import { useUser } from '../UserProvider';
+
+function Admin() {
+  const { user: currentUser } = useUser();
+    const {data, loading, error} = useQuery(GET_USERS);
+    const {colorMode} = useColorMode();
+    const users = data?.users?.filter(user => user.id !== currentUser?.id);
+
+  return (
+    <Flex p={10}>
+      <TableContainer
+        p={5}
+        bg={colorMode === "light" ? "white" : "gray.700"}
+      >
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Username</Th>
+              <Th>Email</Th>
+              <Th>Role</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {users?.map((user) => (
+              <User user={user} />
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Flex>
+  );
+}
+
+export default Admin
