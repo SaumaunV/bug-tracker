5649e39972f5c14f14bc252c223fbd3f233c3b4b
Saumaun <svahedipour1@gmail.com>
2023-04-11 19:30:52 -0500

    Add button to add user modal component

--- components/Projects/AddUserModal.tsx
+++ components/Projects/AddUserModal.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Checkbox,
   CheckboxGroup,
+  Icon,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -11,9 +12,11 @@ import {
   ModalHeader,
   ModalOverlay,
   VStack,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ADD_USERS, GET_PROJECT } from "../../graphql/queries";
+import { MdPersonAddAlt1 } from 'react-icons/md'
 
 interface Props {
   project_id: string;
@@ -25,11 +28,10 @@ interface Props {
         role: string;
       }[]
     | undefined;
-  isOpen: boolean;
-  onClose: () => void;
 }
 
-function AddUserModal({project_id, userList, isOpen, onClose }: Props) {
+function AddUserModal({project_id, userList}: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [users, setUsers] = useState<string[]>([]);
   const [addUsers, { error }] = useMutation(
     ADD_USERS,
@@ -46,36 +48,44 @@ function AddUserModal({project_id, userList, isOpen, onClose }: Props) {
   }
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Add Users</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <CheckboxGroup onChange={(e: string[]) => setUsers(e)}>
-            <VStack spacing={3} alignItems="flex-start">
-              {userList?.map((user) => (
-                <Checkbox key={user.id} value={user.id}>{user.username}</Checkbox>
-              ))}
-            </VStack>
-          </CheckboxGroup>
-        </ModalBody>
-        <ModalFooter>
-          <Button colorScheme="gray" mr={3} onClick={onClose}>
-            Cancel
-          </Button>
-          <Button
-            colorScheme="green"
-            onClick={() => {
-              AddUsers();
-              onClose();
-            }}
-          >
-            Add User
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+    <>
+      <Button onClick={onOpen}>
+        <Icon as={MdPersonAddAlt1} boxSize={6}/>
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add Users</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <CheckboxGroup onChange={(e: string[]) => setUsers(e)}>
+              <VStack spacing={3} alignItems="flex-start">
+                {userList?.map((user) => (
+                  <Checkbox key={user.id} value={user.id}>
+                    {user.username}
+                  </Checkbox>
+                ))}
+              </VStack>
+            </CheckboxGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="gray" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button
+              colorScheme="green"
+              onClick={() => {
+                AddUsers();
+                onClose();
+              }}
+            >
+              Add User
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
