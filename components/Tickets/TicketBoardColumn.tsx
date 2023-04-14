335f80825603bcfb1831999e7e95a1a14ae5f8ef
Saumaun <svahedipour1@gmail.com>
2023-04-13 21:12:21 -0500

    Update style and add tickets to TicketBoardColumn

--- components/Tickets/TicketBoardColumn.tsx
+++ components/Tickets/TicketBoardColumn.tsx
@@ -3,20 +3,31 @@ import React from 'react'
 import TicketCard from './TicketCard';
 
 interface Props {
-    name: string;
+  name: string;
+  tickets:
+    | {
+        __typename?: "Ticket" | undefined;
+        id: string;
+        name: string;
+        description: string;
+        type: string;
+        status: string;
+        priority: string;
+        created_at: string;
+        user_id?: string | null | undefined;
+        project_id: string;
+      }[]
+    | null
+    | undefined;
 }
 
-function TicketBoardColumn({ name }: Props) {
+function TicketBoardColumn({ name, tickets }: Props) {
   return (
-    <Flex
-      w='300px'
-      direction="column"
-      bg="gray.700"
-      borderRadius="md"
-    >
+    <Flex w="300px" direction="column" bg="gray.700" borderRadius="md" h='max-content'>
       <Heading fontSize="sm" m={5} textColor="gray.400">
         {name}
       </Heading>
+      {tickets?.map(ticket => <TicketCard ticket={ticket}/>)}
     </Flex>
   );
 }
