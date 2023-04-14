bb8b7ffdcb20d3915225c3c0dcc4e305746fafd9
Saumaun <svahedipour1@gmail.com>
2023-04-13 20:55:38 -0500

    Add tickets to TicketBoard component

--- components/Tickets/TicketBoard.tsx
+++ components/Tickets/TicketBoard.tsx
@@ -1,14 +1,36 @@
-import { Flex, useColorMode } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import React from 'react'
 import TicketBoardColumn from './TicketBoardColumn';
 
-function TicketBoard() {
+interface Props {
+  tickets:
+    | {
+        __typename?: "Ticket" | undefined;
+        id: string;
+        name: string;
+        description: string;
+        type: string;
+        status: string;
+        priority: string;
+        created_at: string;
+        user_id?: string | null | undefined;
+        project_id: string;
+      }[]
+    | null
+    | undefined;
+}
+
+function TicketBoard({ tickets }: Props) {
+  const newTickets = tickets?.filter(ticket => ticket.status === 'new');
+  const developingTickets = tickets?.filter(ticket => ticket.status === 'development')
+  const testingTickets = tickets?.filter(ticket => ticket.status === 'testing')
+  const resolvedTickets = tickets?.filter(ticket => ticket.status === 'resolved')
   return (
     <Flex gap={16}>
-      <TicketBoardColumn name="NEW" />
-      <TicketBoardColumn name="IN DEVELOPMENT" />
-      <TicketBoardColumn name="TESTING" />
-      <TicketBoardColumn name="RESOLVED" />
+      <TicketBoardColumn name="NEW" tickets={newTickets}/>
+      <TicketBoardColumn name="IN DEVELOPMENT" tickets={developingTickets} />
+      <TicketBoardColumn name="TESTING" tickets={testingTickets} />
+      <TicketBoardColumn name="RESOLVED" tickets={resolvedTickets}/>
     </Flex>
   );
 }
