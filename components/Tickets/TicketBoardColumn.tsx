71231e638c03d2f2176f8f9d830f3c6baf2880e1
Saumaun <svahedipour1@gmail.com>
2023-04-17 17:22:48 -0500

    Add button to create tickets in TicketBoardColumn

--- components/Tickets/TicketBoardColumn.tsx
+++ components/Tickets/TicketBoardColumn.tsx
@@ -1,6 +1,7 @@
-import { Flex, Heading } from '@chakra-ui/react';
-import React from 'react'
+import { Box, Button, Flex, Heading, Textarea } from '@chakra-ui/react';
+import React, { useState } from 'react'
 import TicketCard from './TicketCard';
+import { AddIcon } from '@chakra-ui/icons';
 
 interface Props {
   name: string;
@@ -22,12 +23,48 @@ interface Props {
 }
 
 function TicketBoardColumn({ name, tickets }: Props) {
+  const [newTicket, setNewTicket] = useState(false);
+  const [newTicketName, setNewTicketName ] = useState("");
+
   return (
-    <Flex w="300px" direction="column" bg="gray.700" borderRadius="md" h='max-content'>
-      <Heading fontSize="sm" m={5} textColor="gray.400">
+    <Flex w="300px" direction="column" bg="gray.700" borderRadius="md" p={2}>
+      <Heading fontSize="sm" m={3} textColor="gray.400">
         {name}
       </Heading>
-      {tickets?.map(ticket => <TicketCard ticket={ticket}/>)}
+      {tickets?.map((ticket) => (
+        <TicketCard ticket={ticket} />
+      ))}
+      {name === "NEW" && !newTicket ? (
+        <Button
+          mt={2}
+          rounded="sm"
+          justifyContent="flex-start"
+          alignItems="center"
+          bg={"inherit"}
+          leftIcon={<AddIcon />}
+          onClick={() => setNewTicket(true)}
+        >
+          Create ticket
+        </Button>
+      ) : name === "NEW" && newTicket ? (
+        <Box>
+          <Textarea
+            bg="gray.800"
+            mt={2}
+            value={newTicketName}
+            onChange={(e) => setNewTicketName(e.target.value)}
+            onSubmit={(e) => {
+              e.preventDefault();
+              setNewTicket(false);
+            }}
+            onKeyDown={(e) => {
+              if(e.key === 'Enter'){
+                setNewTicket(false);
+              }
+            }}
+          />
+        </Box>
+      ) : null}
     </Flex>
   );
 }
