eab0afa31a5abcab2beef24ec9b7044c3a95493c
Saumaun <svahedipour1@gmail.com>
2023-04-17 19:05:59 -0500

    Update styling and add username to ticket cards

--- components/Tickets/TicketCard.tsx
+++ components/Tickets/TicketCard.tsx
@@ -1,5 +1,8 @@
-import { Box } from '@chakra-ui/react'
+import { useQuery } from '@apollo/client';
+import { Box, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
+import { GET_USER_NAME } from '../../graphql/queries';
+import { EditIcon, ViewIcon } from '@chakra-ui/icons';
 
 interface Props {
   ticket: {
@@ -28,19 +31,38 @@ const colors: colorType = {
 }
 
 function TicketCard({ ticket }: Props) {
+  const user = useQuery(GET_USER_NAME, { variables: { id: ticket.user_id! } });
   return (
-    <Box
-      bgGradient={`linear(to-b, ${colors[ticket.priority]}.300, ${colors[ticket.priority]}.400)`}
+    <Flex
+      bgGradient={`linear(to-b, ${colors[ticket.priority]}.300, ${
+        colors[ticket.priority]
+      }.400)`}
       textColor="black"
-      borderRadius="sm"
-      m={2}
+      rounded="sm"
+      my={2}
       py={2}
       px={3}
-      fontSize="sm"
       fontWeight={"semibold"}
     >
-      {ticket.name}
-    </Box>
+      <Flex direction="column" w="200px" justifyContent="space-between">
+        <Text mb={8}>{ticket.name}</Text>
+        <Text fontSize="sm" textColor={`${colors[ticket.priority]}.900`}>
+          {user.data?.user?.username}
+        </Text>
+      </Flex>
+
+      <Flex
+        flex={1}
+        direction="column"
+        fontSize="xs"
+        textColor={`${colors[ticket.priority]}.900`}
+        justifyContent="space-between"
+        alignItems="flex-end"
+      >
+        <Box></Box>
+        {ticket.priority.toUpperCase()}
+      </Flex>
+    </Flex>
   );
 }
 
