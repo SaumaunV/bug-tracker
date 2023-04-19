aa4119728eebb53b5d5a58af7de5e3c989b3d436
Saumaun <svahedipour1@gmail.com>
2023-04-18 19:12:14 -0500

    Add create button to ProjectModal component

--- components/Projects/ProjectModal.tsx
+++ components/Projects/ProjectModal.tsx
@@ -3,13 +3,10 @@ import { useState } from 'react';
 import { useMutation } from "@apollo/client";
 import { GET_PROJECTS, CREATE_PROJECT } from '../../graphql/queries';
 import { useUser } from '../../UserProvider';
+import { AddIcon } from '@chakra-ui/icons';
 
-interface Props {
-    isOpen: boolean;
-    onClose: () => void;
-}
-
-function ProjectModal({isOpen, onClose}: Props) {
+function ProjectModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { user, isDemo } = useUser();
     const toast = useToast();
     const [createProject, { error }] = useMutation(CREATE_PROJECT, {
@@ -22,73 +19,83 @@ function ProjectModal({isOpen, onClose}: Props) {
     const [projectDescription, setProjectDescription] = useState('');
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered>
-      <ModalOverlay />
-      <ModalContent
-        as="form"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await createProject({
-            variables: {
-              input: {
-                user_id: user?.id!,
-                name: projectName,
-                description: projectDescription,
-              },
-            },
-          });
-          error
-            ? toast({
-                title: "Error creating project.",
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-              })
-            : toast({
-                title: "Project created successfully.",
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-              });
-          setProjectName("");
-          setProjectDescription("");
-          onClose();
-        }}
+    <>
+      <Button
+        mr={5}
+        rightIcon={<AddIcon />}
+        colorScheme="messenger"
+        onClick={onOpen}
       >
-        <ModalHeader>Create Project</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <FormControl>
-            <FormLabel>Project Name</FormLabel>
-            <Input
-              type="text"
-              isRequired
-              formNoValidate
-              value={projectName}
-              onChange={(e) => setProjectName(e.target.value)}
-              mb={5}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Project Description</FormLabel>
-            <Textarea
-              value={projectDescription}
-              onChange={(e) => setProjectDescription(e.target.value)}
-            />
-          </FormControl>
-        </ModalBody>
-        <ModalFooter>
-          <Button colorScheme={"gray"} mr={3} onClick={onClose}>
-            Cancel
-          </Button>
-          {!isDemo && (
-            <Button type="submit" colorScheme={"blue"}>
-              Create Project
+        New Project
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent
+          as="form"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            await createProject({
+              variables: {
+                input: {
+                  user_id: user?.id!,
+                  name: projectName,
+                  description: projectDescription,
+                },
+              },
+            });
+            error
+              ? toast({
+                  title: "Error creating project.",
+                  status: "error",
+                  duration: 5000,
+                  isClosable: true,
+                })
+              : toast({
+                  title: "Project created successfully.",
+                  status: "success",
+                  duration: 5000,
+                  isClosable: true,
+                });
+            setProjectName("");
+            setProjectDescription("");
+            onClose();
+          }}
+        >
+          <ModalHeader>Create Project</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Project Name</FormLabel>
+              <Input
+                type="text"
+                isRequired
+                formNoValidate
+                value={projectName}
+                onChange={(e) => setProjectName(e.target.value)}
+                mb={5}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Project Description</FormLabel>
+              <Textarea
+                value={projectDescription}
+                onChange={(e) => setProjectDescription(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme={"gray"} mr={3} onClick={onClose}>
+              Cancel
             </Button>
-          )}
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+            {!isDemo && (
+              <Button type="submit" colorScheme={"blue"}>
+                Create Project
+              </Button>
+            )}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
