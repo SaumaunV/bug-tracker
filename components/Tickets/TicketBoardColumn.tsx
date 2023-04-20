146c6acf2bf636d9032feab79c861642396930fa
Saumaun <svahedipour1@gmail.com>
2023-04-19 21:19:02 -0500

    Add functionality to create ticket in board column

--- components/Tickets/TicketBoardColumn.tsx
+++ components/Tickets/TicketBoardColumn.tsx
@@ -1,30 +1,28 @@
-import { Box, Button, Flex, Heading, Textarea } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Select, Textarea, Toast } from '@chakra-ui/react';
 import React, { useState } from 'react'
 import TicketCard from './TicketCard';
 import { AddIcon } from '@chakra-ui/icons';
+import { useMutation } from '@apollo/client';
+import { CREATE_TICKET, GET_PROJECT } from '../../graphql/queries';
+import { Ticket, User } from '../../src/__generated__/graphql';
 
 interface Props {
   name: string;
-  tickets:
-    | {
-        __typename?: "Ticket" | undefined;
-        id: string;
-        name: string;
-        description: string;
-        type: string;
-        status: string;
-        priority: string;
-        created_at: string;
-        user_id?: string | null | undefined;
-        project_id: string;
-      }[]
-    | null
-    | undefined;
+  tickets: Ticket[] | null | undefined;
+  users: User[] | null | undefined;
+  projectId?: string;
 }
 
-function TicketBoardColumn({ name, tickets }: Props) {
+function TicketBoardColumn({ name, tickets, users, projectId }: Props) {
   const [newTicket, setNewTicket] = useState(false);
   const [newTicketName, setNewTicketName ] = useState("");
+  const [ticketType, setTicketType] = useState('bug');
+  const [ticketPriority, setTicketPriority] = useState('low');
+  const [createTicket, { error }] = useMutation(CREATE_TICKET, {
+    refetchQueries: [
+      { query: GET_PROJECT, variables: { id: projectId } },
+    ],
+  });
 
   return (
     <Flex w="300px" direction="column" bg="gray.700" borderRadius="md" p={2}>
@@ -32,7 +30,7 @@ function TicketBoardColumn({ name, tickets }: Props) {
         {name}
       </Heading>
       {tickets?.map((ticket) => (
-        <TicketCard ticket={ticket} />
+        <TicketCard key={ticket.id} ticket={ticket} users={users} />
       ))}
       {name === "NEW" && !newTicket ? (
         <Button
@@ -47,22 +45,72 @@ function TicketBoardColumn({ name, tickets }: Props) {
           Create ticket
         </Button>
       ) : name === "NEW" && newTicket ? (
-        <Box>
+        <Box
+          bg="gray.800"
+          rounded="sm"
+          _focusWithin={{ boxShadow: "0 0 5px 3px royalblue" }}
+        >
           <Textarea
-            bg="gray.800"
             mt={2}
             value={newTicketName}
+            border="none"
+            _focus={{ boxShadow: "none" }}
             onChange={(e) => setNewTicketName(e.target.value)}
-            onSubmit={(e) => {
-              e.preventDefault();
-              setNewTicket(false);
-            }}
-            onKeyDown={(e) => {
-              if(e.key === 'Enter'){
+            onKeyDown={async (e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                await createTicket({
+                  variables: {
+                    input: {
+                      name: newTicketName,
+                      description: "",
+                      type: ticketType,
+                      status: "new",
+                      priority: ticketPriority,
+                      user_id: null,
+                      project_id: projectId!,
+                    },
+                  },
+                });
+                if (error)
+                  Toast({
+                    title: "Error creating ticket",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                  });
                 setNewTicket(false);
+                setNewTicketName("");
               }
             }}
           />
+          <Flex>
+            <Select
+              value={ticketType}
+              onChange={(e) => setTicketType(e.target.value)}
+              size="sm"
+              maxWidth="max-content"
+              border="none"
+              _focus={{ boxShadow: "none" }}
+            >
+              <option value="bug">Bug</option>
+              <option value="feature">Feature</option>
+              <option value="ui">UI</option>
+            </Select>
+            <Select
+              value={ticketPriority}
+              onChange={(e) => setTicketPriority(e.target.value)}
+              size="sm"
+              maxWidth="max-content"
+              border="none"
+              _focus={{ boxShadow: "none" }}
+            >
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+              <option value="immediate">Immediate</option>
+            </Select>
+          </Flex>
         </Box>
       ) : null}
     </Flex>
