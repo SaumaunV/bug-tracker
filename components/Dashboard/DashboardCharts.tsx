5faf1aefef650399d2962993a63758cc2b3621ff
Saumaun <svahedipour1@gmail.com>
2023-05-10 18:29:19 -0500

    Add BarChart component to DashboardCharts

--- components/Dashboard/DashboardCharts.tsx
+++ components/Dashboard/DashboardCharts.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { Flex, useColorMode } from "@chakra-ui/react";
+import { Flex, Skeleton } from "@chakra-ui/react";
 import {
   Chart as ChartJS,
   Tooltip,
@@ -9,12 +9,12 @@ import {
   BarElement,
   Title,
   ArcElement,
-  Legend
+  Legend,
 } from "chart.js";
-import { Bar} from "react-chartjs-2";
 import { GET_USERS, GET_USER_ALL_TICKETS } from "../../graphql/queries";
 import { useUser } from "../../UserProvider";
 import DoughnutChart from "./DoughnutChart";
+import BarChart from "./BarChart";
 
 ChartJS.register(
   CategoryScale,
@@ -57,7 +57,6 @@ const Colors = {
 
 function DashboardCharts() {
   const { user } = useUser();
-  const { colorMode } = useColorMode();
   const { data, loading, error } = useQuery(GET_USER_ALL_TICKETS, {variables: {id: user ? user.id : ""}});
   const users = useQuery(GET_USERS);
 
@@ -72,137 +71,60 @@ function DashboardCharts() {
   let uiCount = 0;
   data?.user?.allTickets?.forEach(ticket => ticket.type === "feature" ? featureCount++ : ticket.type === 'bug' ? bugCount++ : uiCount++);
 
-  const options = {
-    plugins: {
-      title: {
-        display: true,
-        text: "Tickets by Priority",
-        color: colorMode === "dark" ? "white" : "",
-        font: { size: 16 },
-      },
-      legend: { display: false },
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        grid: {
-          display: false,
-        },
-        ticks: { color: colorMode === "dark" ? "white" : "" },
-      },
-      y: {
-        grid: {
-          display: false,
-        },
-        ticks: { color: colorMode === "dark" ? "white" : "" },
-      },
-    },
-  };
+  const priorityData = TicketPriorities.map(
+    (priority) =>
+      data?.user?.allTickets?.filter(
+        (ticket) => ticket.priority === priority.toLowerCase()
+      ).length!
+  );
 
-  if (loading) return <h1>Loading...</h1>;
+  const ticketStatusData = TicketStatuses.map(
+    (status) =>
+      data?.user?.allTickets?.filter(
+        (ticket) => ticket.status === status.toLowerCase()
+      ).length!
+  );
+
+  if (loading) return <Skeleton m={10} h='300px'></Skeleton>;
   if (error) return <h1>Error</h1>;
   return (
     <div>
-      <Flex m={10} justify='space-between' gap={20}>
-        <Flex
-          flex={1}
-          minWidth='500px'
-          minHeight="300px"
-          shadow="md"
-          border={colorMode === "light" ? "1px solid" : ""}
-          borderColor="gray.100"
-          bg={colorMode === "dark" ? "gray.700" : "white"}
-          rounded="md"
-          p="5"
-        >
-          <Bar
-            data={{
-              labels: TicketPriorities,
-              datasets: [
-                {
-                  label: "Number of Tickets",
-                  data: TicketPriorities.map(
-                    (priority) =>
-                      data?.user?.allTickets?.filter(
-                        (ticket) => ticket.priority === priority.toLowerCase()
-                      ).length
-                  ),
-                  borderWidth: colorMode === "light" ? 2 : 0,
-                  borderColor: ["purple", "red", "orange", "green"],
-                  backgroundColor:
-                    colorMode === "light"
-                      ? [Colors.purple, Colors.red, Colors.orange, Colors.green]
-                      : [
-                          Colors.purpleDark,
-                          Colors.redDark,
-                          Colors.orangeDark,
-                          Colors.greenDark,
-                        ],
-                },
-              ],
-            }}
-            options={options}
-          />
-        </Flex>
-        <Flex
-          flex={1}
-          minWidth='500px'
-          minHeight="300px"
-          shadow="md"
-          border={colorMode === "light" ? "1px solid" : ""}
-          borderColor="gray.100"
-          bg={colorMode === "dark" ? "gray.700" : "white"}
-          rounded="md"
-          p="5"
-        >
-          <Bar
-            data={{
-              labels: TicketStatuses,
-              datasets: [
-                {
-                  label: "Number of Tickets",
-                  data: TicketStatuses.map(
-                    (status) =>
-                      data?.user?.allTickets?.filter(
-                        (ticket) => ticket.status === status.toLowerCase()
-                      ).length
-                  ),
-                  borderWidth: colorMode === "light" ? 2 : 0,
-                  borderColor: ["grey", "blue", "yellow", "orange", "green"],
-                  backgroundColor:
-                    colorMode === "light"
-                      ? [
-                          Colors.grey,
-                          Colors.blue,
-                          Colors.yellow,
-                          Colors.orange,
-                          Colors.green,
-                        ]
-                      : [
-                          Colors.greyDark,
-                          Colors.blueDark,
-                          Colors.yellowDark,
-                          Colors.orangeDark,
-                          Colors.greenDark,
-                        ],
-                },
-              ],
-            }}
-            options={{
-              ...options,
-              plugins: {
-                title: {
-                  display: true,
-                  text: "Tickets by Status",
-                  color: colorMode === "dark" ? "white" : "",
-                  font: { size: 16 },
-                },
-                legend: { display: false },
-              },
-            }}
-          />
-        </Flex>
+      <Flex m={10} justify="space-between" gap={20}>
+        <BarChart
+          labels={TicketPriorities}
+          title="Tickets by Priority"
+          data={priorityData}
+          colors={[
+            [Colors.purple, Colors.red, Colors.orange, Colors.green],
+            [
+              Colors.purpleDark,
+              Colors.redDark,
+              Colors.orangeDark,
+              Colors.greenDark,
+            ],
+          ]}
+        />
+        <BarChart
+          labels={TicketStatuses}
+          title="Tickets by Status"
+          data={ticketStatusData}
+          colors={[
+            [
+              Colors.grey,
+              Colors.blue,
+              Colors.yellow,
+              Colors.orange,
+              Colors.green,
+            ],
+            [
+              Colors.greyDark,
+              Colors.blueDark,
+              Colors.yellowDark,
+              Colors.orangeDark,
+              Colors.greenDark,
+            ],
+          ]}
+        />
       </Flex>
       <Flex p={10}>
         <DoughnutChart
@@ -210,10 +132,10 @@ function DashboardCharts() {
           label={"Number of Tickets"}
           data={[featureCount, bugCount, uiCount]}
           title="Tickets by Type"
-          first        
+          first
         />
         <DoughnutChart
-          labels={['Admin', 'Developer']}
+          labels={["Admin", "Developer"]}
           label={"Number of Users"}
           data={[adminCount, developerCount]}
           title="Users by Role"
