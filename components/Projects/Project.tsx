cf59f13f1897e6a77eb6ba29e365f53903e8e739
Saumaun <svahedipour1@gmail.com>
2023-05-11 21:47:38 -0500

    Remove delete button from project card

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -4,12 +4,10 @@ import {
   CardFooter,
   Heading,
   useColorMode,
-  useDisclosure,
 } from "@chakra-ui/react";
 import { Card } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
-import AlertDialogDelete from "../AlertDialogDelete";
 
 interface Props {
   id: string;
@@ -18,38 +16,31 @@ interface Props {
 }
 
 function Project({ id, name }: Props) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const { colorMode } = useColorMode();
 
   return (
-    <>
-      <Card
-        data-testid="project-component"
-        align="center"
-        maxWidth="300px"
-        w="300px"
-        mr={10}
-        mb={10}
-        h="180px"
-        bg={colorMode === "light" ? "white" : ""}
-      >
-        <Heading size="md" mt={3}>
-          {name}
-        </Heading>
-        <CardBody></CardBody>
-        <CardFooter>
-          <Link href={"projects/[projectId]"} as={`projects/${id}`}>
-            <Button colorScheme="blue" mr={5}>
-              Details
-            </Button>
-          </Link>
-          <Button colorScheme="red" onClick={onOpen}>
-            Delete
+    <Card
+      data-testid="project-component"
+      align="center"
+      maxWidth="300px"
+      w="300px"
+      mr={10}
+      mb={10}
+      h="180px"
+      bg={colorMode === "light" ? "white" : ""}
+    >
+      <Heading size="md" mt={3}>
+        {name}
+      </Heading>
+      <CardBody></CardBody>
+      <CardFooter>
+        <Link href={"projects/[projectId]"} as={`projects/${id}`}>
+          <Button colorScheme="blue" mr={5}>
+            Details
           </Button>
-        </CardFooter>
-      </Card>
-      <AlertDialogDelete id={id} isOpen={isOpen} onClose={onClose} type='project' title='Delete Project' />
-    </>
+        </Link>
+      </CardFooter>
+    </Card>
   );
 }
 
